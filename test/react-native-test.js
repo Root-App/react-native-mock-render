0a64c6cbcc4531c7f28e20d4c7182d1a912eae67
Bob Carson <bob@carvinesoftware.com>
2016-12-28 17:50:10 -0500

    Adds tests for renderable components being functions.

---
+++ test/react-native-test.js
@@ -0,0 +1,69 @@
+import { expect } from 'chai';
+import { get } from 'lodash';
+
+describe('react-native.js', () => {
+  it('returns renderable types when expected', () => {
+    const MockReactNative = require('../src/react-native');
+
+    const renderableComponents = [
+      'ActivityIndicator',
+      'ActivityIndicatorIOS',
+      'ART.LinearGradient',
+      'ART.RadialGradient',
+      'ART.Pattern',
+      'ART.Transform',
+      'ART.Path',
+      'ART.Surface',
+      'ART.Group',
+      'ART.ClippingRectangle',
+      'ART.Shape',
+      'ART.Text',
+      'Button',
+      'DatePickerIOS',
+      'DrawerLayoutAndroid',
+      'Image',
+      'ImageEditor',
+      'ImageStore',
+      'KeyboardAvoidingView',
+      'ListView',
+      'MapView',
+      'Modal',
+      'Navigator',
+      'NavigatorIOS',
+      // 'Picker',
+      // 'Picker.Item',
+      'PickerIOS',
+      'ProgressBarAndroid',
+      'ProgressViewIOS',
+      'ScrollView',
+      'SegmentedControlIOS',
+      'SliderIOS',
+      'SnapshotViewIOS',
+      'Switch',
+      'PullToRefreshViewAndroid',
+      'RecyclerViewBackedScrollView',
+      'RefreshControl',
+      'StatusBar',
+      'SwitchAndroid',
+      'SwitchIOS',
+      // 'TabBarIOS',
+      // 'TabBarIOS.Item',
+      'Text',
+      'TextInput',
+      'ToastAndroid',
+      'ToolbarAndroid',
+      'Touchable',
+      'TouchableHighlight',
+      'TouchableNativeFeedback',
+      'TouchableOpacity',
+      'TouchableWithoutFeedback',
+      'View',
+      'ViewPagerAndroid',
+      'WebView',
+    ];
+
+    renderableComponents.forEach((component) => {
+      expect(get(MockReactNative, component)).to.be.a('function');
+    });
+  });
+});

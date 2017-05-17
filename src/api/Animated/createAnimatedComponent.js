4cd43757b26a8b347aab3e49e7969439f7fd9f86
Bob Carson <bob@carvinesoftware.com>
2017-05-17 18:00:40 -0400

    Updates createAnimatedComponent to deeply render.

--- src/api/Animated/createAnimatedComponent.js
+++ src/api/Animated/createAnimatedComponent.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
 function createAnimatedComponent(Component) {
   const refName = 'node';
 
   class AnimatedComponent extends React.Component {
+    static propTypes = {
+      children: PropTypes.node,
+    }
+
     render() {
       return (
         <Component
           ref={refName}
-        />
+        >
+          {this.props.children}
+        </Component>
       );
     }
   }

f945a92e3ab3a56f1845694f095df38b71f0482e
silentcloud <rjmuqiang@gmail.com>
2017-09-27 18:12:40 +0800

    fix: test utils

--- src/react-native.js
+++ src/react-native.js
@@ -101,14 +101,13 @@ const ReactNative = {
   NavigationExperimental: require('./Libraries/NavigationExperimental'),
 };
 
-
 // See http://facebook.github.io/react/docs/addons.html
 const ReactNativeAddons = {
   // LinkedStateMixin: require('react-addons-linked-state-mixin') deprecated,
   Perf: require('react-addons-perf'),
   PureRenderMixin: require('react-addons-pure-render-mixin'),
   TestModule: require('./NativeModules/TestModule'),
-  TestUtils: require('react-addons-test-utils'),
+  TestUtils: require('react-dom/test-utils'),
   // TODO(lmr): not sure where to find this
   // batchedUpdates: require('ReactUpdates').batchedUpdates, deprecated
   // cloneWithProps: require('react-addons-clone-with-props'), deprecated

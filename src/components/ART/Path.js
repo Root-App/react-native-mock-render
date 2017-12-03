5de8d8457e4977aee7499405fe4d05b7a2ac1e49
Marlena Bowen <marlena@joinroot.com>
2017-12-03 10:57:02 -0500

    Updates ART Path

--- src/components/ART/Path.js
+++ src/components/ART/Path.js
@@ -1,28 +1,36 @@
+var Class = require('art/core/class.js');
+var Path = require('art/core/path.js');
 
-class Path {
-  constructor(path) {
-    [
-      'push',
-      'reset',
-      'move',
-      'moveTo',
-      'line',
-      'lineTo',
-      'curve',
-      'curveTo',
-      'arc',
-      'arcTo',
-      'counterArc',
-      'counterArcTo',
-      'close'
-    ].forEach((methodName) => { this[methodName] = () => this; });
+var MOVE_TO = 0;
+var CLOSE = 1;
+var LINE_TO = 2;
+var CURVE_TO = 3;
+var ARC = 4;
 
-    this.path = path || [];
+var SerializablePath = Class(Path, {
+
+  initialize: function(path) {
+  },
+
+  onReset: function() {
+  },
+
+  onMove: function(sx, sy, x, y) {
+  },
+
+  onLine: function(sx, sy, x, y) {
+  },
+
+  onBezierCurve: function(sx, sy, p1x, p1y, p2x, p2y, x, y) {
+  },
+
+  onArc: function(sx, sy, ex, ey, cx, cy, rx, ry, sa, ea, ccw, rotation) {
+  },
+
+  onClose: function() {
+  },
+
+  toJSON: function() {
   }
 
-  toJSON() {
-    return JSON.stringify(this.path);
-  }
-}
-
-module.exports = Path;
+});

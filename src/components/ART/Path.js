9107d8d816d5cf68e3b06bded73561f59dd70f44
Ben Wagaman <ben@joinroot.com>
2018-03-06 13:17:20 -0500

    fix linter

--- src/components/ART/Path.js
+++ src/components/ART/Path.js
@@ -1,38 +1,15 @@
-var Class = require('art/core/class.js');
-var Path = require('art/core/path.js');
+const className = require('art/core/class.js');
+const Path = require('art/core/path.js');
 
-var MOVE_TO = 0;
-var CLOSE = 1;
-var LINE_TO = 2;
-var CURVE_TO = 3;
-var ARC = 4;
-
-var SerializablePath = Class(Path, {
-
-  initialize: function(path) {
-  },
-
-  onReset: function() {
-  },
-
-  onMove: function(sx, sy, x, y) {
-  },
-
-  onLine: function(sx, sy, x, y) {
-  },
-
-  onBezierCurve: function(sx, sy, p1x, p1y, p2x, p2y, x, y) {
-  },
-
-  onArc: function(sx, sy, ex, ey, cx, cy, rx, ry, sa, ea, ccw, rotation) {
-  },
-
-  onClose: function() {
-  },
-
-  toJSON: function() {
-  }
-
+const SerializablePath = className(Path, {
+  initialize: (path) => {},
+  onReset: () => {},
+  onMove: (sx, sy, x, y) => {},
+  onLine: (sx, sy, x, y) => {},
+  onBezierCurve: (sx, sy, p1x, p1y, p2x, p2y, x, y) => {},
+  onArc: (sx, sy, ex, ey, cx, cy, rx, ry, sa, ea, ccw, rotation) => {},
+  onClose: () => {},
+  toJSON: () => {},
 });
 
 module.exports = SerializablePath;

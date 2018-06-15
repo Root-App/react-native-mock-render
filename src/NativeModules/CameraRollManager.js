b0c0d140f1dcf51fdd1e38fe800b2e851b27270e
Ivan <ivan.bacallado@blueskytechnology.eu>
2018-06-15 08:46:54 +0100

    Add CameraRollManager mock data for getPhotos method

--- src/NativeModules/CameraRollManager.js
+++ src/NativeModules/CameraRollManager.js
@@ -1,13 +1,53 @@
-
 const CameraRollManager = {
   saveImageWithTag(imageTag) {
     return Promise.resolve(['/asset/url']);
   },
   getPhotos(params) {
-    return Promise.resolve([
-      // TODO(lmr):
-    ]);
-  },
+    return Promise.resolve({
+      edges: [
+        {
+          node: {
+            type: 'image/jpeg',
+            group_name: 'Camera',
+            image: {
+              uri: 'content://media/external/images/media/1',
+              height: 2448,
+              width: 3968
+            },
+            timestamp: 1528972673375
+          }
+        },
+        {
+          node: {
+            type: 'image/jpeg',
+            group_name: 'Camera',
+            image: {
+              uri: 'content://media/external/images/media/2',
+              height: 2448,
+              width: 3968
+            },
+            timestamp: 1528972673375
+          }
+        },
+        {
+          node: {
+            type: 'image/jpeg',
+            group_name: 'Camera',
+            image: {
+              uri: 'content://media/external/images/media/3',
+              height: 2448,
+              width: 3968
+            },
+            timestamp: 1528972673375
+          }
+        }
+      ],
+      page_info: {
+        has_next_page: true,
+        end_cursor: '1528919312601'
+      }
+    });
+  }
 };
 
 module.exports = CameraRollManager;
